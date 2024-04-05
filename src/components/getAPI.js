2448169c4839de7022e6979f59762fe5ab586b0f
Andrew <andrewsheu0617@gmail.com>
2024-04-05 17:01:13 +1100

    React Router update.1

--- src/components/getAPI.js
+++ src/components/getAPI.js
@@ -1,46 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import useFetch from "../Hooks/useFetch";
 
 const CryptoPrices = () => {
-    const [cryptoPrices, setCryptoPrices] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [cryptoPrices, setCryptoPrices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchCryptoPrices = async () => {
-            const headers = {
-                'X-CoinAPI-Key': '95D5E0CA-2C7D-4CC8-B697-3B466C68E9E8'
-            };
-            try {
-                const response = await fetch('https://rest.coinapi.io/v1/exchangerate', { headers });
-                const data = await response.json();
-                setCryptoPrices(data);
-                setIsLoading(false);
-            } catch (error) {
-                console.error('Failed to fetch data:', error);
-                setIsLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchCryptoPrices = async () => {
+      const headers = {
+        "X-CoinAPI-Key": "95D5E0CA-2C7D-4CC8-B697-3B466C68E9E8",
+      };
+      try {
+        const response = await fetch(
+          "https://rest.coinapi.io/v1/exchangerate",
+          { headers }
+        );
+        const data = await response.json();
+        setCryptoPrices(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+        setIsLoading(false);
+      }
+    };
 
-        fetchCryptoPrices();
+    fetchCryptoPrices();
+  }, []);
 
-    }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            <h1>Crypto Prices</h1>
-            <ul>
-                {cryptoPrices.map((crypto) => (
-                    <li key={crypto.asset_id}>
-                        <h2>{crypto.name} ({crypto.asset_id})</h2>
-                        <p>Price USD: ${crypto.price_usd}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Crypto Prices</h1>
+      <ul>
+        {cryptoPrices.map((crypto) => (
+          <li key={crypto.asset_id}>
+            <h2>
+              {crypto.name} ({crypto.asset_id})
+            </h2>
+            <p>Price USD: ${crypto.price_usd}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CryptoPrices;
---
+++ src/components/price.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+const Price = () => {
+  return <div></div>;
+};
+
+export default Price;
